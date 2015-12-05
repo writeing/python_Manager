453609cfe47a54cf25cb82b6afe85e635f358ae7
王选超 <write_1>
2015-12-05 12:46:10 +0800

    No 1
    12-5 12:45
    添加了数据库，还有部分数据库文件，

---
+++ src/var.py
@@ -0,0 +1,17 @@
+'''
+Created on 2015-12-5
+
+@author: write_1
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
